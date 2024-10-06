ee118e3874d7c6cccf9d903c70039fd068699d4e
Andrei_Gridin <andreigridin918@gmail.com>
2024-10-06 21:03:49 +0500

    Создал методы meth_swap_arr и show_ar

--- Pointers_2/Pointers_2.cpp
+++ Pointers_2/Pointers_2.cpp
@@ -1,6 +1,34 @@
 ﻿#include <iostream>
 
+void meth_swap_arr(int* arr, int* c)
+{
+	for (int i = 0; i < *c / 2; i++)
+	{
+		*(arr + i) += *(arr + *c - 1 - i);
+		*(arr + *c - 1 - i) = *(arr + i) - *(arr + *c - 1 - i);
+		*(arr + i) -= *(arr + *c - 1 - i);
+	}
+}
+
+void show_ar(int* num, int* c)
+{
+	for (int i = 0; i < *c; i++)
+	{
+		std::cout << ' ' << *(num + i) ;
+	}
+	std::cout << std::endl;
+}
+
 int main()
 {
-    int ar[] = { 1,2,3,4,5,6,7,8,9 };
+    int ar[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int size_ar = std::size(ar);
+
+	show_ar(ar, &size_ar);
+
+	meth_swap_arr(ar, &size_ar);
+
+	show_ar(ar, &size_ar);
+
+	return 0;
 }
